1313e114d556f1efd8ad306d60d8bb8b9cad154f
Satyam Yadav <satyamyadav3@gmail.com>
2020-04-07 16:21:27 +0530

    feat: adds changelog

---
+++ scripts/version.sh
@@ -0,0 +1,4 @@
+LOG=$(git log $(git describe --tags --abbrev=0 @^)..@ --pretty=format:"%h %s" --no-merges | sed -e 's/^/* /')
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+DATE=$(date +%F)
+echo "v$PACKAGE_VERSION  ($DATE) \n$LOG \n-------------------\n\n" >> CHANGELOG.md
