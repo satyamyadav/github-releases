30c7da1600c0c266ea3347c34fd53ae93531781d
innosatyam <85342175+innosatyam@users.noreply.github.com>
2021-07-15 15:00:39 +0530

    chore: updates github actions for master

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,4 +2,4 @@ LOG=$(git log $(git describe --tags --abbrev=0 @^)..@ --pretty=format:"%h %s" --
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 DATE=$(date +%F)
 echo "\n-------------------\n **v$PACKAGE_VERSION**  ($DATE) \n\n$LOG" >> CHANGELOG.md &&
-git add CHANGELOG.md
+git add .
